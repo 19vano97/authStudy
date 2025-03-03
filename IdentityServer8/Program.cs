4709e47534767b0f6d87acb31b5f14873c6b3fce
19vano97 <vanillin97@gmail.com>
2025-03-03 22:09:23 +0200

    nfnfg

--- IdentityServer8/Program.cs
+++ IdentityServer8/Program.cs
@@ -1,54 +1,28 @@
-﻿using IdentityServer4.Stores;
-using IdentityServerMvc.Configs;
-using IdentityServerMvc.Data;
-using IdentityServerMvc.Models.Account;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddControllers();
-builder.Services.AddDbContext<IdentityServerMvcDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-builder.Services.AddSingleton<IClientStore, IdentityServerConfig>();
-builder.Services.AddIdentity<Account, IdentityRole>()
-    .AddEntityFrameworkStores<IdentityServerMvcDbContext>()
-    .AddDefaultTokenProviders();
-builder.Services.AddIdentityServer()
-    .AddAspNetIdentity<Account>()
-    .AddInMemoryIdentityResources(IdentityServerConfig.IdentityResources)
-    .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
-    .AddInMemoryClients(IdentityServerConfig.Clients)
-    .AddDeveloperSigningCredential();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // app.UseExceptionHandler("/Home/Error");
-    // // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseIdentityServer();
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
+
 app.UseAuthorization();
-app.MapStaticAssets();
-app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
