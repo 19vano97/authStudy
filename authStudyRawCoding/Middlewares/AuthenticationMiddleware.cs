de11d962515b8eeaba3e75a4d26b7c4a075d742a
19vano97 <vanillin97@gmail.com>
2025-03-03 22:08:09 +0200

    wefwef

--- authStudyRawCoding/Middlewares/AuthenticationMiddleware.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace authStudyRawCoding.Middlewares;
-
-public class AuthenticationMiddleware
-{
-    
-}
--- authStudyRawCoding/Services/IAuthService.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace authStudyRawCoding.Services;
-
-public interface IAuthService
-{
-
-}
